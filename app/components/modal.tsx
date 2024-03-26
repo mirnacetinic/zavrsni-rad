fa8cb7169499f34f69843a73ecc9506ccd7c1beb
mirnacetinic <mirnacetinic16@gmail.com>
2024-03-26 10:24:54 +0100

    updates to modal

--- app/components/modal.tsx
+++ app/components/modal.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React from "react";
+import React, { useState, ChangeEvent } from "react";
 import { AiOutlineClose } from "react-icons/ai";
 
 interface ModalProps {
@@ -8,30 +8,22 @@ interface ModalProps {
   content: string;
 }
 
-function Modal({ isOpen, onClose, content}: ModalProps) {
-  const showContent = () => {
-    switch (content) {
-      case "login":
-        return (
-          <>
-            <h3 className="text-2xl font-bold text-gray-900">Login</h3>
-            <div className="mt-2 px-7 py-3">
-              <p className="text-lg text-gray-500">Login form goes here...</p>
-            </div>
-          </>
-        );
-      case "signup":
-        return (
-          <>
-            <h3 className="text-2xl font-bold text-gray-900">Sign Up</h3>
-            <div className="mt-2 px-7 py-3">
-              <p className="text-lg text-gray-500">Sign up form goes here...</p>
-            </div>
-          </>
-        );
-      default:
-        return null;
-    }
+const Modal = ({ isOpen, onClose, content }: ModalProps) => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  });
+
+  const handleInputChange = (e:ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value
+    }));
+  };
+
+  const handleLogin = () => {
+    console.log(formData);
   };
 
   return (
@@ -42,11 +34,28 @@ function Modal({ isOpen, onClose, content}: ModalProps) {
           <div className="fixed inset-0 flex items-center justify-center z-50 animate-slideInDown">
             <div className="p-8 border w-96 shadow-lg rounded-md bg-white relative">
               <div className="absolute text-black top-2 right-2 cursor-pointer" onClick={onClose}>
-                <AiOutlineClose/>
+                <AiOutlineClose />
               </div>
               <div className="text-center">
-                {showContent()}
-                <div className="flex justify-center mt-4">
+                <h3 className="text-2xl font-bold text-gray-900">{content === "login" ? "Login" : "Sign Up"}</h3>
+                <div className="text-black mt-6">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    value={formData.email}
+                    onChange={handleInputChange}
+                    className="block w-full px-4 py-2 border rounded-md focus:outline-none focus:border-purple-300"/>
+                  <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    value={formData.password}
+                    onChange={handleInputChange}
+                    className="mt-4 block w-full px-4 py-2 border rounded-md focus:outline-none focus:border-gray-300"/>
+                  <button onClick={handleLogin} className="mt-6 w-full px-4 py-2 bg-purple-800 text-white rounded-md hover:bg-gray-300 focus:outline-none focus:bg-gray-600">
+                    {content === "login" ? "Login" : "Sign Up"}
+                  </button>
                 </div>
               </div>
             </div>
@@ -55,6 +64,6 @@ function Modal({ isOpen, onClose, content}: ModalProps) {
       )}
     </div>
   );
-}
+};
 
 export default Modal;
